6f6f0e014e185ccfb46ad3930fbefa6e76acb427
praveen <prav.developer08@gmail.com>
2021-09-23 20:08:48 +0530

    initial Commit

---
+++ src/main/java/com/sb/docker/app/SbDockerAppApplication.java
@@ -0,0 +1,13 @@
+package com.sb.docker.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbDockerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbDockerAppApplication.class, args);
+	}
+
+}
